695695911bff5ab93f9c5aa0cff01533a1d276ab
kvxd <redslyice@outlook.de>
2026-05-09 18:47:56 +0200

    chore: initial commit

---
+++ src/main/kotlin/org/kvxd/carbon/Carbon.kt
@@ -0,0 +1,9 @@
+package org.kvxd.carbon
+
+import net.fabricmc.api.ModInitializer
+
+class Carbon : ModInitializer {
+
+    override fun onInitialize() {
+    }
+}
---
+++ src/main/kotlin/org/kvxd/carbon/client/CarbonClient.kt
@@ -0,0 +1,9 @@
+package org.kvxd.carbon.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class CarbonClient : ClientModInitializer {
+
+    override fun onInitializeClient() {
+    }
+}
